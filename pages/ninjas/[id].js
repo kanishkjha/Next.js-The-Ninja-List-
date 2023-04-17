dd881947ea292df6e698263fc20bef4d34c3b4b8
unknown <kanishkjha10@gmail.com>
2023-04-17 08:16:39 +0530

    first repo

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,30 @@
+export const getStaticPaths = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+
+}
+
+const Details = () => {
+    return (
+        <div>
+            <h1>Details Page</h1>
+        </div>
+    )
+}
+
+export default Details
